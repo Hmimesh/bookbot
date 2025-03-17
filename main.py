81b88e44750cedf88d8f86b2f76ba86b24d3b546
Hmimesh <highshot135@gmail.com>
2025-03-17 14:41:17 +0200

    added some more books

--- main.py
+++ main.py
@@ -3,15 +3,19 @@ from stats import (
     chars_dict_to_sorted_list,
     get_chars_dict,
 )
-
+import sys
 
 def main():
-    book_path = "books/frankenstein.txt"
-    text = get_book_text(book_path)
-    num_words = get_num_words(text)
-    chars_dict = get_chars_dict(text)
-    chars_sorted_list = chars_dict_to_sorted_list(chars_dict)
-    print_report(book_path, num_words, chars_sorted_list)
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    else:
+        book_path = sys.argv[1]
+        text = get_book_text(book_path)
+        num_words = get_num_words(text)
+        chars_dict = get_chars_dict(text)
+        chars_sorted_list = chars_dict_to_sorted_list(chars_dict)
+        print_report(book_path, num_words, chars_sorted_list)
 
 
 def get_book_text(path):
